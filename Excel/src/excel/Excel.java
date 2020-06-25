4f1dd33b719f9867cf29ca2053118412c2e2e071
Estefania Aguilar <silestagui@gmail.com>
2020-06-25 16:26:53 -0300

    Excel

--- Excel/src/excel/Excel.java
+++ Excel/src/excel/Excel.java
@@ -1,10 +1,19 @@
 
 package excel;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import modelo.Conexion;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
@@ -12,7 +21,10 @@ public class Excel {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //crearExcel();
+        //cargarExcel_a_BD();
+        cargarBD_a_Excel();
+        
     }
     
     public static void crearExcel(){
@@ -25,8 +37,99 @@ public class Excel {
         }catch(Exception ex){
             System.err.println("Error "+ ex);
         }
+    }
+    
+    public static void cargarExcel_a_BD(){
+        Conexion con = new Conexion();
+        PreparedStatement ps = null;
+        
+        try{
+            Connection conexion = con.getConnection();
+            FileInputStream archivo = new FileInputStream(new File("C:\\home\\shinee\\Documentos\\Productos.xlsx"));
+            XSSFWorkbook librolectura = new XSSFWorkbook(archivo);
+            XSSFSheet hojaLectura = librolectura.getSheetAt(0);
+            
+            int numFilas = hojaLectura.getLastRowNum();
+            
+            System.out.println(numFilas);
+            
+            for (int i = 1; i < 10; i++) {
+                Row fila = hojaLectura.getRow(i);
+                
+                ps = conexion.prepareStatement("insert into producto (codigo, nombre, precio, cantidad) values (?,?,?,?)");
+                ps.setString(1, fila.getCell(0).getStringCellValue());
+                ps.setString(2, fila.getCell(1).getStringCellValue());
+                ps.setDouble(3, fila.getCell(2).getNumericCellValue());
+                ps.setInt(4,(int) fila.getCell(3).getNumericCellValue());
+                
+                ps.executeUpdate();
+            }
+            
+            conexion.close();
+            
+            
+        
+        }catch(Exception ex){
+            System.err.println("Error "+ ex);
+        }
+    }
+    
+    public static void cargarBD_a_Excel(){
+        Workbook libro = new XSSFWorkbook();
+        Sheet hoja = libro.createSheet("Reporte productos");
+        Conexion con = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         
         
+        
+        String[] cabeceras = new String[]{"Código","Nombre","Precio","Cantidad"};
+        
+        Row filaCabeceras = hoja.createRow(0);
+        
+        for (int i = 0; i < cabeceras.length; i++) {
+            Cell celda = filaCabeceras.createCell(i);
+            celda.setCellValue(cabeceras[i]);       
+ 
+        }
+        
+        int numFila = 1;
+        
+        
+        try{
+            Connection conexion = con.getConnection();
+            ps = conexion.prepareStatement("select codigo, nombre, precio, cantidad from producto");
+            rs = ps.executeQuery();
+            
+            int numCol = rs.getMetaData().getColumnCount();
+            
+            while(rs.next()){
+                Row filaDatos = hoja.createRow(numFila);
+                
+                for (int i = 0; i <numCol; i++) {
+                    Cell celda = filaDatos.createCell(i);
+                    
+                    if(i == 0  || i == 1){
+                        celda.setCellValue(rs.getString(i + 1));    
+                    }else{
+                        celda.setCellValue(rs.getDouble(i + 1));
+                    }
+                }
+                
+                numFila++;
+                
+            
+            }
+            FileOutputStream archivo = new FileOutputStream("ReporteProductos.xlsx");
+            libro.write(archivo);
+            archivo.close();
+            
+        }catch(Exception ex){
+            System.err.println("Error "+ ex);
+        }
+        
+    
+    
     }
     
 }
