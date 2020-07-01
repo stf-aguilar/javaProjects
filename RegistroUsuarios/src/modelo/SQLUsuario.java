b7b6ba42e0ffa2f11d382b7abd329744175fdc21
Estefania Aguilar <silestagui@gmail.com>
2020-07-01 19:26:59 -0300

    Registro usuarios desde form a BD

--- RegistroUsuarios/src/modelo/SQLUsuario.java
+++ RegistroUsuarios/src/modelo/SQLUsuario.java
@@ -8,13 +8,23 @@ import java.sql.PreparedStatement;
 public class SQLUsuario {
     public boolean RegistrarUsuario(Usuario usuario){
         Conexion con = new Conexion();
-        Connection conexion = con.getConnection();
         PreparedStatement ps = null;
         
         try{
-        
+            Connection conexion = con.getConnection();         
+            ps = conexion.prepareStatement("insert into usuario (nombreUsuario, contraseña,nombre, correo,idTipo_Usuario) values(?,?,?,?,?)");
+            ps.setString(1, usuario.getNombreUsuario());
+            ps.setString(2, usuario.getContraseña());
+            ps.setString(3, usuario.getNombre());
+            ps.setString(4,usuario.getCorreo());
+            ps.setInt(5,usuario.getIdTipo_usuario());
+            
+            ps.executeUpdate();
+            
+            return true;
         }catch(Exception ex){
             System.err.println("Error "+ ex);
+            return false;
         }
     
     
--- RegistroUsuarios/src/modelo/Usuario.java
+++ RegistroUsuarios/src/modelo/Usuario.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author shinee
- */
 public class Usuario {
+    private int id;
+    private String nombreUsuario;
+    private String contraseña;
+    private String nombre;
+    private String correo;
+    private String ultima_sesion;
+    private int idTipo_usuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUltima_sesion() {
+        return ultima_sesion;
+    }
+
+    public void setUltima_sesion(String ultima_sesion) {
+        this.ultima_sesion = ultima_sesion;
+    }
+
+    public int getIdTipo_usuario() {
+        return idTipo_usuario;
+    }
+
+    public void setIdTipo_usuario(int idTipo_usuario) {
+        this.idTipo_usuario = idTipo_usuario;
+    }
+    
     
 }
